954fca37a35f1226e68bf8e762dc5e94db3060b4
MajorFoxs_OvO <mckekejun@gmail.com>
2025-03-05 01:26:21 +0800

    更新密钥提示

--- assets/js/config.js
+++ assets/js/config.js
@@ -280,100 +280,100 @@ const CONFIG = {
             {
                 name: "IntelliJ IDEA",
                 icon: "https://resources.jetbrains.com.cn/storage/products/company/brand/logos/IntelliJ_IDEA_icon.png",
-                versions: ["2024.1.x","2023.1.x","2022.1.x","2021.1.x"],
+                versions: ["2024.x.x","2023.x.x","2022.x.x","2021.x.x"],
                 keys: {
-                    "2024.x.x": "无需密钥",
-                    "2023.x.x": "无需密钥",
-                    "2022.x.x": "无需密钥",
-                    "2021.x.x": "无需密钥",
+                    "2024.x.x": "无需密钥 | No key required",
+                    "2023.x.x": "无需密钥 | No key required",
+                    "2022.x.x": "无需密钥 | No key required",
+                    "2021.x.x": "无需密钥 | No key required",
                 }
             },
             {
                 name: "PyCharm",
                 icon: "https://resources.jetbrains.com.cn/storage/products/company/brand/logos/PyCharm_icon.png",
-                versions: ["2024.1.x","2023.1.x","2022.1.x","2021.1.x"],
+                versions: ["2024.x.x","2023.x.x","2022.x.x","2021.x.x"],
                 keys: {
-                    "2024.x.x": "无需密钥",
-                    "2023.x.x": "无需密钥",
-                    "2022.x.x": "无需密钥",
-                    "2021.x.x": "无需密钥",
+                    "2024.x.x": "无需密钥 | No key required",
+                    "2023.x.x": "无需密钥 | No key required",
+                    "2022.x.x": "无需密钥 | No key required",
+                    "2021.x.x": "无需密钥 | No key required",
                 }
             },
             {
                 name: "WebStorm",
                 icon: "https://resources.jetbrains.com.cn/storage/products/company/brand/logos/WebStorm_icon.png",
-                versions: ["2024.1.x","2023.1.x","2022.1.x","2021.1.x"],
+                versions: ["2024.x.x","2023.x.x","2022.x.x","2021.x.x"],
                 keys: {
-                    "2024.x.x": "无需密钥",
-                    "2023.x.x": "无需密钥",
-                    "2022.x.x": "无需密钥",
-                    "2021.x.x": "无需密钥",
+                    "2024.x.x": "无需密钥 | No key required",
+                    "2023.x.x": "无需密钥 | No key required",
+                    "2022.x.x": "无需密钥 | No key required",
+                    "2021.x.x": "无需密钥 | No key required",
                 }
             },
             {
                 name: "PhpStorm",
                 icon: "https://resources.jetbrains.com.cn/storage/products/company/brand/logos/PhpStorm_icon.png",
-                versions: ["2024.1.x","2023.1.x","2022.1.x","2021.1.x"],
+                versions: ["2024.x.x","2023.x.x","2022.x.x","2021.x.x"],
                 keys: {
-                    "2024.x.x": "无需密钥",
-                    "2023.x.x": "无需密钥",
-                    "2022.x.x": "无需密钥",
-                    "2021.x.x": "无需密钥",
+                    "2024.x.x": "无需密钥 | No key required",
+                    "2023.x.x": "无需密钥 | No key required",
+                    "2022.x.x": "无需密钥 | No key required",
+                    "2021.x.x": "无需密钥 | No key required",
                 }
             },
             {
                 name: "Rider",
                 icon: "https://resources.jetbrains.com.cn/storage/products/company/brand/logos/Rider_icon.png",
-                versions: ["2024.1.x","2023.1.x","2022.1.x","2021.1.x"],
+                versions: ["2024.x.x","2023.x.x","2022.x.x","2021.x.x"],
                 keys: {
-                    "2024.x.x": "无需密钥",
-                    "2023.x.x": "无需密钥",
-                    "2022.x.x": "无需密钥",
-                    "2021.x.x": "无需密钥",
+                    "2024.x.x": "无需密钥 | No key required",
+                    "2023.x.x": "无需密钥 | No key required",
+                    "2022.x.x": "无需密钥 | No key required",
+                    "2021.x.x": "无需密钥 | No key required",
                 }
             },
             {
                 name: "CLion",
                 icon: "https://resources.jetbrains.com.cn/storage/products/company/brand/logos/CLion_icon.png",
-                versions: ["2024.1.x","2023.1.x","2022.1.x","2021.1.x"],
+                versions: ["2024.x.x","2023.x.x","2022.x.x","2021.x.x"],
                 keys: {
-                    "2024.x.x": "无需密钥",
-                    "2023.x.x": "无需密钥",
-                    "2022.x.x": "无需密钥",
-                    "2021.x.x": "无需密钥",
+                    "2024.x.x": "无需密钥 | No key required",
+                    "2023.x.x": "无需密钥 | No key required",
+                    "2022.x.x": "无需密钥 | No key required",
+                    "2021.x.x": "无需密钥 | No key required",
                 }
             },
             {
                 name: "GoLand",
                 icon: "https://resources.jetbrains.com.cn/storage/products/company/brand/logos/GoLand_icon.png",
-                versions: ["2024.1.x","2023.1.x","2022.1.x","2021.1.x"],
+                versions: ["2024.x.x","2023.x.x","2022.x.x","2021.x.x"],
                 keys: {
-                    "2024.x.x": "无需密钥",
-                    "2023.x.x": "无需密钥",
-                    "2022.x.x": "无需密钥",
-                    "2021.x.x": "无需密钥",
+                    "2024.x.x": "无需密钥 | No key required",
+                    "2023.x.x": "无需密钥 | No key required",
+                    "2022.x.x": "无需密钥 | No key required",
+                    "2021.x.x": "无需密钥 | No key required",
                 }
             },
             {
                 name: "DataGrip",
                 icon: "https://resources.jetbrains.com.cn/storage/products/company/brand/logos/DataGrip_icon.png",
-                versions: ["2024.1.x","2023.1.x","2022.1.x","2021.1.x"],
+                versions: ["2024.x.x","2023.x.x","2022.x.x","2021.x.x"],
                 keys: {
-                    "2024.x.x": "无需密钥",
-                    "2023.x.x": "无需密钥",
-                    "2022.x.x": "无需密钥",
-                    "2021.x.x": "无需密钥",
+                    "2024.x.x": "无需密钥 | No key required",
+                    "2023.x.x": "无需密钥 | No key required",
+                    "2022.x.x": "无需密钥 | No key required",
+                    "2021.x.x": "无需密钥 | No key required",
                 }
             },
             {
                 name: "DataSpell",
                 icon: "https://resources.jetbrains.com.cn/storage/products/company/brand/logos/DataSpell_icon.png",
-                versions: ["2024.1.x","2023.1.x","2022.1.x","2021.1.x"],
+                versions: ["2024.x.x","2023.x.x","2022.x.x","2021.x.x"],
                 keys: {
-                    "2024.x.x": "无需密钥",
-                    "2023.x.x": "无需密钥",
-                    "2022.x.x": "无需密钥",
-                    "2021.x.x": "无需密钥",
+                    "2024.x.x": "无需密钥 | No key required",
+                    "2023.x.x": "无需密钥 | No key required",
+                    "2022.x.x": "无需密钥 | No key required",
+                    "2021.x.x": "无需密钥 | No key required",
                 }
             },
             {
